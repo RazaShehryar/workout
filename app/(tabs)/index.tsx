b33a2e4cd2684a06bdbd5c81300c134617ff3c30
Shehryar Raza <42263739+RazaShehryar@users.noreply.github.com>
2024-07-10 20:37:18 +0500

    added changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,11 @@
 import { WorkoutData, createTable, getLatestWorkout, insertData } from "@/database";
-import { addMinutesToDate, capitalizeFirstLetter } from "@/utils";
+import { addMinutesToDate, capitalizeFirstLetter, locations, workoutType } from "@/utils";
 import HealthKit, {
   HKQuantityTypeIdentifier,
   HKUnits,
-  HKWorkoutActivityType,
   HKWorkoutRouteTypeIdentifier,
   HKWorkoutTypeIdentifier,
+  UnitOfEnergy,
   UnitOfLength,
 } from "@kingstinct/react-native-healthkit";
 import { Picker } from "@react-native-picker/picker";
@@ -13,73 +13,6 @@ import { Pedometer } from "expo-sensors";
 import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import Modal from "react-native-modal";
-
-const locations = [
-  {
-    latitude: 45.4215,
-    longitude: -75.6972,
-    altitude: 70.0,
-    timestamp: new Date("2024-07-09T10:00:00Z").getTime(),
-    speed: 10.5,
-    speedAccuracy: 1.0,
-    horizontalAccuracy: 5.0, // realistic value for horizontal accuracy in meters
-    verticalAccuracy: 10.0, // realistic value for vertical accuracy in meters
-    course: 45.0, // realistic value for course in degrees
-  },
-  {
-    latitude: 45.4231,
-    longitude: -75.6958,
-    altitude: 72.5,
-    timestamp: new Date("2024-07-09T10:05:00Z").getTime(),
-    speed: 12.0,
-    speedAccuracy: 0.8,
-    horizontalAccuracy: 5.0,
-    verticalAccuracy: 10.0,
-    course: 50.0,
-  },
-  {
-    latitude: 45.425,
-    longitude: -75.6943,
-    altitude: 75.0,
-    timestamp: new Date("2024-07-09T10:10:00Z").getTime(),
-    speed: 11.2,
-    speedAccuracy: 1.2,
-    horizontalAccuracy: 5.0,
-    verticalAccuracy: 10.0,
-    course: 55.0,
-  },
-  {
-    latitude: 45.4268,
-    longitude: -75.6929,
-    altitude: 78.0,
-    timestamp: new Date("2024-07-09T10:15:00Z").getTime(),
-    speed: 13.5,
-    speedAccuracy: 0.9,
-    horizontalAccuracy: 5.0,
-    verticalAccuracy: 10.0,
-    course: 60.0,
-  },
-  {
-    latitude: 45.4285,
-    longitude: -75.6914,
-    altitude: 80.5,
-    timestamp: new Date("2024-07-09T10:20:00Z").getTime(),
-    speed: 14.0,
-    speedAccuracy: 0.7,
-    horizontalAccuracy: 5.0,
-    verticalAccuracy: 10.0,
-    course: 65.0,
-  },
-];
-
-const workoutType = (type: string) => {
-  if (type === "running") {
-    return HKWorkoutActivityType.running;
-  } else if (type === "cycling") {
-    return HKWorkoutActivityType.cycling;
-  }
-  return HKWorkoutActivityType.walking;
-};
 
 const App = () => {
   const [isModalVisible, setModalVisible] = useState(false);
@@ -117,6 +50,7 @@ const App = () => {
             HKQuantityTypeIdentifier.distanceCycling,
             HKQuantityTypeIdentifier.distanceWalkingRunning,
             HKQuantityTypeIdentifier.stepCount,
+            HKQuantityTypeIdentifier.activeEnergyBurned,
             HKWorkoutTypeIdentifier,
             HKWorkoutRouteTypeIdentifier,
           ]
@@ -161,33 +95,49 @@ const App = () => {
 
       const workoutOptions = {
         end: addMinutesToDate(new Date(), 60),
-        totals: { energyBurned: Number(calories), distance: Number(distance) * 1000 },
+        totals: {
+          energyBurned: Number(calories),
+          distance: Number(distance) * 1000,
+        },
       };
 
-      await HealthKit.saveQuantitySample(HKQuantityTypeIdentifier.stepCount, HKUnits.Count, currentStepCount, {
-        start,
-        end,
-      });
+      const steps = await HealthKit.saveQuantitySample(
+        HKQuantityTypeIdentifier.stepCount,
+        HKUnits.Count,
+        currentStepCount,
+        { start, end }
+      );
 
-      await HealthKit.saveQuantitySample(
+      const userDistance = await HealthKit.saveQuantitySample(
         type === "cycling" ? HKQuantityTypeIdentifier.distanceCycling : HKQuantityTypeIdentifier.distanceWalkingRunning,
         UnitOfLength.Meter,
         Number(distance) * 1000,
         { start, end }
       );
 
+      const energyBurned = await HealthKit.saveQuantitySample(
+        HKQuantityTypeIdentifier.activeEnergyBurned,
+        UnitOfEnergy.SmallCalories,
+        Number(calories),
+        { start, end }
+      );
+
+      await Promise.all([energyBurned, steps, userDistance]);
+
       const workoutResult = await HealthKit.saveWorkoutSample(workoutType(type), [], new Date(), workoutOptions);
 
       if (workoutResult) {
         console.log("workout created: ", workoutResult);
-
-        await HealthKit.saveWorkoutRoute(workoutResult, locations);
-        insertData({ ...options, id: workoutResult } as unknown as WorkoutData, fetchLatestWorkout);
-        setDistance("");
-        setCalories("");
-        setType("walking");
-        setCurrentStepCount(100);
-        setModalVisible(false);
+        const workoutRouteResult = await HealthKit.saveWorkoutRoute(workoutResult, locations);
+        if (workoutRouteResult) {
+          console.log("workout route created");
+          insertData({ ...options, id: workoutResult } as unknown as WorkoutData, fetchLatestWorkout);
+          setDistance("");
+          setCalories("");
+          setType("walking");
+          setCurrentStepCount(100);
+          setModalVisible(false);
+        }
       }
     } catch (err) {
       console.log(err);
--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { HKWorkoutActivityType } from "@kingstinct/react-native-healthkit";
+
 export function addMinutesToDate(date: Date, minutes: number): Date {
   return new Date(date.getTime() + minutes * 60000);
 }
@@ -5,3 +7,70 @@ export function addMinutesToDate(date: Date, minutes: number): Date {
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export const workoutType = (type: string) => {
+  if (type === "running") {
+    return HKWorkoutActivityType.running;
+  } else if (type === "cycling") {
+    return HKWorkoutActivityType.cycling;
+  }
+  return HKWorkoutActivityType.walking;
+};
+
+export const locations = [
+  {
+    latitude: 45.4215,
+    longitude: -75.6972,
+    altitude: 70.0,
+    timestamp: new Date("2024-07-09T10:00:00Z").getTime(),
+    speed: 10.5,
+    speedAccuracy: 1.0,
+    horizontalAccuracy: 5.0, // realistic value for horizontal accuracy in meters
+    verticalAccuracy: 10.0, // realistic value for vertical accuracy in meters
+    course: 45.0, // realistic value for course in degrees
+  },
+  {
+    latitude: 45.4231,
+    longitude: -75.6958,
+    altitude: 72.5,
+    timestamp: new Date("2024-07-09T10:05:00Z").getTime(),
+    speed: 12.0,
+    speedAccuracy: 0.8,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 50.0,
+  },
+  {
+    latitude: 45.425,
+    longitude: -75.6943,
+    altitude: 75.0,
+    timestamp: new Date("2024-07-09T10:10:00Z").getTime(),
+    speed: 11.2,
+    speedAccuracy: 1.2,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 55.0,
+  },
+  {
+    latitude: 45.4268,
+    longitude: -75.6929,
+    altitude: 78.0,
+    timestamp: new Date("2024-07-09T10:15:00Z").getTime(),
+    speed: 13.5,
+    speedAccuracy: 0.9,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 60.0,
+  },
+  {
+    latitude: 45.4285,
+    longitude: -75.6914,
+    altitude: 80.5,
+    timestamp: new Date("2024-07-09T10:20:00Z").getTime(),
+    speed: 14.0,
+    speedAccuracy: 0.7,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 65.0,
+  },
+];
