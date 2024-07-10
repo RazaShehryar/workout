3b6df1ca09bf832c2ea351d79a2a0b19d8261b73
Shehryar Raza <42263739+RazaShehryar@users.noreply.github.com>
2024-07-10 18:17:57 +0500

    replaced react-native-health with react-native-healthkit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,85 @@
 import { WorkoutData, createTable, getLatestWorkout, insertData } from "@/database";
 import { addMinutesToDate, capitalizeFirstLetter } from "@/utils";
+import HealthKit, {
+  HKQuantityTypeIdentifier,
+  HKUnits,
+  HKWorkoutActivityType,
+  HKWorkoutRouteTypeIdentifier,
+  HKWorkoutTypeIdentifier,
+  UnitOfLength,
+} from "@kingstinct/react-native-healthkit";
 import { Picker } from "@react-native-picker/picker";
+import { Pedometer } from "expo-sensors";
 import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
-import AppleHealthKit, { HealthActivityOptions, HealthKitPermissions } from "react-native-health";
 import Modal from "react-native-modal";
-import { Pedometer } from "expo-sensors";
 
-const permissions = {
-  permissions: {
-    write: [
-      AppleHealthKit.Constants.Permissions.Workout,
-      AppleHealthKit.Constants.Permissions.Steps,
-      AppleHealthKit.Constants.Permissions.StepCount,
-    ],
+const locations = [
+  {
+    latitude: 45.4215,
+    longitude: -75.6972,
+    altitude: 70.0,
+    timestamp: new Date("2024-07-09T10:00:00Z").getTime(),
+    speed: 10.5,
+    speedAccuracy: 1.0,
+    horizontalAccuracy: 5.0, // realistic value for horizontal accuracy in meters
+    verticalAccuracy: 10.0, // realistic value for vertical accuracy in meters
+    course: 45.0, // realistic value for course in degrees
   },
-} as HealthKitPermissions;
+  {
+    latitude: 45.4231,
+    longitude: -75.6958,
+    altitude: 72.5,
+    timestamp: new Date("2024-07-09T10:05:00Z").getTime(),
+    speed: 12.0,
+    speedAccuracy: 0.8,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 50.0,
+  },
+  {
+    latitude: 45.425,
+    longitude: -75.6943,
+    altitude: 75.0,
+    timestamp: new Date("2024-07-09T10:10:00Z").getTime(),
+    speed: 11.2,
+    speedAccuracy: 1.2,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 55.0,
+  },
+  {
+    latitude: 45.4268,
+    longitude: -75.6929,
+    altitude: 78.0,
+    timestamp: new Date("2024-07-09T10:15:00Z").getTime(),
+    speed: 13.5,
+    speedAccuracy: 0.9,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 60.0,
+  },
+  {
+    latitude: 45.4285,
+    longitude: -75.6914,
+    altitude: 80.5,
+    timestamp: new Date("2024-07-09T10:20:00Z").getTime(),
+    speed: 14.0,
+    speedAccuracy: 0.7,
+    horizontalAccuracy: 5.0,
+    verticalAccuracy: 10.0,
+    course: 65.0,
+  },
+];
+
+const workoutType = (type: string) => {
+  if (type === "running") {
+    return HKWorkoutActivityType.running;
+  } else if (type === "cycling") {
+    return HKWorkoutActivityType.cycling;
+  }
+  return HKWorkoutActivityType.walking;
+};
 
 const App = () => {
   const [isModalVisible, setModalVisible] = useState(false);
@@ -39,19 +103,27 @@ const App = () => {
 
   useEffect(() => {
     createTable();
-    AppleHealthKit.initHealthKit(permissions, (err, results) => {
-      if (err) {
-        console.log("error initializing Healthkit: ", err);
-        setIsReady(false);
-        return;
-      }
-      setIsReady(true);
-      console.log("Healthkit initialized: ", results);
-    });
     fetchLatestWorkout();
     let subscription: Pedometer.Subscription;
     (async () => {
       try {
+        const isHealthDataAvailable = await HealthKit.isHealthDataAvailable();
+        if (!isHealthDataAvailable) {
+          return;
+        }
+        const result = await HealthKit.requestAuthorization(
+          [],
+          [
+            HKQuantityTypeIdentifier.distanceCycling,
+            HKQuantityTypeIdentifier.distanceWalkingRunning,
+            HKQuantityTypeIdentifier.stepCount,
+            HKWorkoutTypeIdentifier,
+            HKWorkoutRouteTypeIdentifier,
+          ]
+        );
+        if (result) {
+          setIsReady(true);
+        }
         const isAvailable = await Pedometer.isAvailableAsync();
         console.log("is available ", isAvailable);
         if (!isAvailable) {
@@ -67,53 +139,59 @@ const App = () => {
         console.log(e);
       }
     })();
-    // return () => subscription?.remove();
+
+    return () => subscription?.remove();
   }, []);
 
-  const saveStepsToHealthKit = () => {
-    const startDate = new Date().toISOString();
-    const endDate = addMinutesToDate(new Date(), 60).toISOString();
+  const saveStepsToHealthKit = async () => {
+    try {
+      const start = new Date();
+      const end = addMinutesToDate(new Date(), 60);
 
-    let options: Record<string, unknown> = {
-      type: capitalizeFirstLetter(type),
-      startDate,
-      endDate,
-      energyBurned: Number(calories),
-      energyBurnedUnit: "calorie",
-      distance: Number(distance) * 1000,
-      distanceUnit: "meter",
-    };
+      let options: Record<string, unknown> = {
+        type: capitalizeFirstLetter(type),
+        startDate: start.toISOString(),
+        endDate: end.toISOString(),
+        energyBurned: Number(calories),
+        energyBurnedUnit: "calorie",
+        distance: Number(distance) * 1000,
+        distanceUnit: "meter",
+        steps: currentStepCount,
+      };
 
-    const stepsData = { value: currentStepCount, startDate, endDate };
+      const workoutOptions = {
+        end: addMinutesToDate(new Date(), 60),
+        totals: { energyBurned: Number(calories), distance: Number(distance) * 1000 },
+      };
 
-    let steps = currentStepCount;
+      await HealthKit.saveQuantitySample(HKQuantityTypeIdentifier.stepCount, HKUnits.Count, currentStepCount, {
+        start,
+        end,
+      });
 
-    AppleHealthKit.saveSteps(stepsData, (err, results) => {
-      if (err) {
-        steps = 0;
-        console.log("error saving steps to Healthkit: ", err);
-        return;
-      }
-      console.log("Steps saved to Healthkit: ", results.toString());
-    });
+      await HealthKit.saveQuantitySample(
+        type === "cycling" ? HKQuantityTypeIdentifier.distanceCycling : HKQuantityTypeIdentifier.distanceWalkingRunning,
+        UnitOfLength.Meter,
+        Number(distance) * 1000,
+        { start, end }
+      );
 
-    AppleHealthKit.saveWorkout(options as unknown as HealthActivityOptions, (err, results) => {
-      if (err) {
-        console.log("error saving workout to Healthkit: ", err);
-        return;
-      }
-      const id = results.toString();
-      options = { ...options, id, steps };
+      const workoutResult = await HealthKit.saveWorkoutSample(workoutType(type), [], new Date(), workoutOptions);
 
-      if (id) {
-        insertData(options as unknown as WorkoutData, fetchLatestWorkout);
+      if (workoutResult) {
+        console.log("workout created: ", workoutResult);
+
+        await HealthKit.saveWorkoutRoute(workoutResult, locations);
+        insertData({ ...options, id: workoutResult } as unknown as WorkoutData, fetchLatestWorkout);
         setDistance("");
         setCalories("");
         setType("walking");
         setCurrentStepCount(100);
         setModalVisible(false);
       }
-    });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   if (!isReady) {
